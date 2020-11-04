c2c505b40075dd37ed910d054afdfb1811dabb89
MashaYudi <mashayudi@gmail.com>
2020-11-04 16:19:41 +0100

    add challenge 23

--- challenge3_23.go
+++ challenge3_23.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/robquant/cryptopals/pkg/tools"
 )
 
 const (
@@ -23,20 +25,91 @@ const (
 	lowerBitsOfW = ((1 << w) - 1)
 )
 
-func InvertRightShiftOp(y, l int32) {
-	res := int32(0)
-
-	// 1010
-	// 0101
-	// 1111
-
+func IsolateBit(val uint32, bit uint8) uint32 {
+	return val & (1 << bit)
 }
 
+// Revert y = y ^ ((y >> shift) & a)
+func RevertRightShiftOp(y uint32, shift uint8, a uint32) uint32 {
+	res := uint32(0)
+	for i := uint8(0); i < shift; i++ {
+		res |= IsolateBit(y, 31-i)
+	}
+	for i := shift; i < 32; i++ {
+		unshifted := IsolateBit(res, 31-i+shift)
+		res |= IsolateBit(y, 31-i) ^ ((unshifted >> shift) & a)
+	}
+	return res
+}
+
+// Revert y = y ^ ((y << shift) & a)
+func RevertLeftShiftOp(y uint32, shift uint8, a uint32) uint32 {
+	res := uint32(0)
+	for i := uint8(0); i < shift; i++ {
+		res |= IsolateBit(y, i)
+	}
+	for i := shift; i < 32; i++ {
+		unshifted := IsolateBit(res, i-shift)
+		res |= IsolateBit(y, i) ^ ((unshifted << shift) & a)
+	}
+	return res
+}
+
+//shift = 2
+//	x2 x3 x4 x5 0  0
+// &
+//	a0 a1 a2 a3 a4 a5
+// ^
+//	x0 x1 x2 x3 x4 x5
+// -------------------
+//  y0 y1 y2 y3 y4 y5
+
+//res[i] = y[i]
+//res[i] = y[i] ^ (res[i+shift] & a[i])
+
+// Revert y = y ^ ((y << shift) & a)
+func RevertLeftShiftOp2(y uint32, shift uint8, a uint32) uint32 {
+	res := uint32(0)
+	numOfChunks := 32 / shift
+
+	//mask with ones(length = shift) in the last position  000011
+	baseMask := uint32(0xffffffff >> (32 - shift))
+
+	for i := uint8(0); i <= numOfChunks; i++ {
+		//mask shifted to the next bits of interest  i = 0 : 000011 ; i = 1 : 001100
+		maskForChunk := uint32(baseMask << (shift * i))
+
+		//new portion of final result
+		chunk := y & maskForChunk // y4 y5 for i == 0
+
+		//y[i] ^ (res[i+shift] & a[i])
+		y ^= (chunk << shift) & a // y4 y5 (= x4 x5) shifted left for i == 0
+
+		res |= chunk
+	}
+	return res
+}
+
+// y := mt.MT[mt.index]
+// y = y ^ ((y >> u) & d)
+// y = y ^ ((y << s) & b)
+// y = y ^ ((y << t) & c)
+// y = y ^ (y >> l)
+
 func main() {
-	fmt.Println("Ch3/23---------------")
-	// y = y ^ ((y >> u) & d)
-	// y = y ^ ((y << s) & b)
-	// y = y ^ ((y << t) & c)
-	// y = y ^ (y >> l)
-	// return lowerBitsOfW & y
+	rng := tools.NewMT19937Rng(100)
+	rng2 := tools.NewMT19937Rng(138400)
+	for i := 0; i < 624; i++ {
+		rng2.Random()
+		y := uint32(rng.Random())
+		y = RevertRightShiftOp(y, l, ^uint32(0))
+		y = RevertLeftShiftOp2(y, t, c)
+		y = RevertLeftShiftOp2(y, s, b)
+		y = RevertRightShiftOp(y, u, d)
+		rng2.MT[i] = int(y)
+	}
+
+	for i := 0; i < 10; i++ {
+		fmt.Printf("Orig: %d, Copy: %d\n", rng.Random(), rng2.Random())
+	}
 }
